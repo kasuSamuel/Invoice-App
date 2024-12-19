a0e38a1e4d794570359c23227ff169b431dd7606
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-19 21:24:05 +0000

    nav-sidebar implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+      RouterOutlet, 
+      SideNavbarComponent
+    ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Invoice-App';
