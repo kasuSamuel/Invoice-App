39a28206c43463e3b695e32d72c66c0807f447da
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-23 11:49:52 +0000

    Header design completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavbarComponent } from './side-navbar/side-navbar.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
       RouterOutlet, 
-      SideNavbarComponent
+      SideNavbarComponent,
+      HeaderComponent
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
