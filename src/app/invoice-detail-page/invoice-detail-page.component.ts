5b6d66f353f94b5e7ada6de6522792e58f313abd
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-26 13:32:50 +0000

    card-details component implemented.

--- src/app/invoice-detail-page/invoice-detail-page.component.ts
+++ src/app/invoice-detail-page/invoice-detail-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { IconComponent } from "../icon/icon.component";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-invoice-detail-page',
   standalone: true,
-  imports: [],
+  imports: [IconComponent, ButtonComponent],
   templateUrl: './invoice-detail-page.component.html',
   styleUrl: './invoice-detail-page.component.css'
 })
