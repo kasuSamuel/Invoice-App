09dea66c103d8b3d825c378413a9443cba881d6c
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-30 17:09:27 +0000

    mroe dialog design implemented.

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeadlineComponent } from '../headline/headline.component';
+import { TextComponent } from '../text/text.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeadlineComponent, TextComponent, ButtonComponent],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
 export class DialogComponent {
+  showConfirmDialog = true;
 
+  cancel() {
+    this.showConfirmDialog = false;
+    
+  }
+
+  delete() {
+    console.log('Invoice deleted');
+    this.showConfirmDialog = false;
+  }
 }
--- src/app/invoice-detail-page/invoice-detail-page.component.ts
+++ src/app/invoice-detail-page/invoice-detail-page.component.ts
@@ -6,17 +6,19 @@ import { Invoice } from '../shared/invoice.interface';
 import { DataService } from '../shared/data.service';
 import { TextComponent } from "../text/text.component";
 import { HeadlineComponent } from "../headline/headline.component";
+import { DialogComponent } from "../dialog/dialog.component";
 
 @Component({
   selector: 'app-invoice-detail-page',
   standalone: true,
-  imports: [ButtonComponent, IconComponent, CommonModule, TextComponent, HeadlineComponent],
+  imports: [ButtonComponent, IconComponent, CommonModule, TextComponent, HeadlineComponent, DialogComponent],
   templateUrl: './invoice-detail-page.component.html',
   styleUrl: './invoice-detail-page.component.css'
 })
 export class InvoiceDetailPageComponent {
   selectedInvoice: Invoice | null = null;
-  
+  showDialog: boolean = false;
+
   constructor(private location: Location,private dataService: DataService) {
   }
 
@@ -25,9 +27,15 @@ export class InvoiceDetailPageComponent {
     this.dataService.getSelectedInvoice().subscribe(invoice => {
       this.selectedInvoice = invoice;
     });
+
+    
   }
   goBack() {
     this.location.back(); 
-    console.log('object is back');
   }
+  openDialog(): void {
+    this.showDialog = true;
+    // document.body.style.overflow = 'hidden';
+  }
+
 }
