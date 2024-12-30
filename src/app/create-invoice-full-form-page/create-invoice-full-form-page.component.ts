7943a05ee50ccc3039d15a69be2200b2252baa3c
Kasu Samuel <kasusamuel17@gmail.com>
2024-12-30 14:55:32 +0000

    forms functions implemented.

--- src/app/create-invoice-full-form-page/create-invoice-full-form-page.component.ts
+++ src/app/create-invoice-full-form-page/create-invoice-full-form-page.component.ts
@@ -6,46 +6,78 @@ import { CommonModule } from '@angular/common';
 import { CalenderComponent } from "../calender/calender.component";
 import { InputFieldComponent } from "../input-field/input-field.component";
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-invoice-full-form-page',
   standalone: true,
-  imports: [HeadlineComponent, ButtonComponent, CommonModule, CalenderComponent, InputFieldComponent, FormsModule,],
+  imports: [ReactiveFormsModule, HeadlineComponent, ButtonComponent, CommonModule, CalenderComponent, InputFieldComponent, FormsModule],
   templateUrl: './create-invoice-full-form-page.component.html',
-  styleUrl: './create-invoice-full-form-page.component.css'
+  styleUrls: ['./create-invoice-full-form-page.component.css']
 })
-export class CreateInvoiceFullFormPageComponent implements OnInit{
-  streetAddress: string = '';
-  city: string = '';
-  postcode: string = '';
-  country: string = '';
-  clientName: string = '';
-  clientEmail: string = '';
-  clientAddress: string = '';
-  billToCity: string = '';
-  billToPostcode: string = '';
-  billToCountry: string = '';
-  paymentTerms: string = '';
-  projectDescription: string = '';
+export class CreateInvoiceFullFormPageComponent implements OnInit {
 
-  constructor(private dataService: DataService) {}
+  invoiceForm: FormGroup = this.fb.group({});  // Initialize here
 
-  sidebarVisible: boolean = false; // Track sidebar visibility
+  constructor(private dataService: DataService, private fb: FormBuilder) { }
+
+  sidebarVisible: boolean = false;
+
   ngOnInit(): void {
+    // Initialize theme and sidebar visibility from the service
     this.dataService.initTheme();
-
-    // Subscribe to the sidebar visibility changes
     this.dataService.sidebarVisibility$.subscribe((visible) => {
       this.sidebarVisible = visible;
     });
+
+    // Initialize the form
+    this.invoiceForm = this.fb.group({
+      billFrom: this.fb.group({
+        streetAddress: ['', Validators.required],
+        city: ['', Validators.required],
+        postcode: ['', Validators.required],
+        country: ['', Validators.required],
+      }),
+      billTo: this.fb.group({
+        clientName: ['', Validators.required],
+        clientEmail: ['', [Validators.required, Validators.email]],
+        clientAddress: ['', Validators.required],
+        billToCity: ['', Validators.required],
+        billToPostcode: ['', Validators.required],
+        billToCountry: ['', Validators.required],
+      }),
+      paymentTerms: ['Select', Validators.required],
+      projectDescription: ['', Validators.required],
+      items: this.fb.array([])  // Dynamic array for items
+    });
   }
 
-
-
-  discardChanges() {
-    let confirmation = window.confirm("Are you sure you want to discard the changes?");
-  }
+  // Getter for the items FormArray
+  get items() {
+    return (this.invoiceForm.get('items') as FormArray);
   }
 
+  // Method to add a new item to the items array
+  addItem() {
+    const itemForm = this.fb.group({
+      itemName: ['', Validators.required],
+      quantity: ['', [Validators.required, Validators.min(1)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+    });
+    this.items.push(itemForm);
+  }
 
+  // Method to remove an item from the items array
+  removeItem(index: number) {
+    this.items.removeAt(index);
+  }
 
+  // Form submission handler
+  onSubmit() {
+    if (this.invoiceForm.valid) {
+      console.log(this.invoiceForm.value);  // Log form data or send to a service
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
--- src/app/input-field/input-field.component.ts
+++ src/app/input-field/input-field.component.ts
@@ -1,20 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './input-field.component.html',
-  styleUrl: './input-field.component.css'
+  styleUrls: ['./input-field.component.css']
 })
 export class InputFieldComponent {
-
-  @Input() labelFor: string = '';       // This will bind to the 'for' attribute of label
-  @Input() labelText: string = '';      // This will be the label text
-  @Input() inputId: string = '';        // This will be the id of the input element
-  @Input() inputType: string = 'text';  // This will define the input type
-  @Input() placeholder: string = '';    // This will be the placeholder for the input field
-  @Input() value: string = ''; 
+  @Input() label: string = '';
+  @Input() formGroup: FormGroup | null = null;
+  @Input() controlName: string = '';
+  @Input() type: string = 'text'; // Default type is text
 }
